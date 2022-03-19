5d5c6d4bf776881262f4da6aa6a96a6646d02a0a
rule-airport <101653181+rule-airport@users.noreply.github.com>
2022-03-19 11:24:08 +0800

    Update exchange.js

--- QuantumultX/exchange.js
+++ QuantumultX/exchange.js
@@ -1,8 +1,8 @@
 /**
  * 监控汇率变化
  * @author: Peng-YM
- * 修改人@rule-airport
- * 更新地址：https://raw.githubusercontent.com/Peng-YM/QuanX/master/Tasks/exchange.js
+ * 修改人@author: rule-airport
+ * 更新地址：
  * 配置方法：
  * 1. 设置基准货币，默认人民币(CNY)。
  * 2. 设置保留几位小数。
